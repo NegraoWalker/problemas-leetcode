fc05bd5897253475cb86ba8e4c147af6ef761b1c
Walker Negrão <c0662240@vale.com>
2025-02-05 16:21:15 -0300

    Atualização

--- strings/roman-to-integer/main.go
+++ strings/roman-to-integer/main.go
@@ -1,9 +1,30 @@
 package main
 
-func romanToInt(s string) int {
+import "fmt"
 
+func romanToInt(s string) int {
+	values := map[byte]int{'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+	sum := 0
+	for i := 0; i < len(s); i++ {
+		if i+1 < len(s) && values[s[i]] < values[s[i+1]] {
+			sum -= values[s[i]]
+		} else {
+			sum += values[s[i]]
+		}
+	}
+	return sum
 }
 
 func main() {
+	s := "III"
+	fmt.Println(romanToInt(s))
 
+	s = "LVIII"
+	fmt.Println(romanToInt(s))
+
+	s = "MCMXCIV"
+	fmt.Println(romanToInt(s))
+
+	s = "IV"
+	fmt.Println(romanToInt(s))
 }
